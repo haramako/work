8ef4a478a11253f09399cbb6fcf4fefe3678bfec
Harada Makoto <haramako@gmail.com>
2012-04-20 01:44:41 +0900

    .

---
+++ janyuhai/simple_require.js
@@ -0,0 +1,24 @@
+/*
+node.jsのｑrequireの真似をブラウザで行うためのコード.
+対象パッケージのjsは事前に読み込んだ状態で、node.jsのrequireと同様に利用する。
+
+使用例:
+ 
+<script src="./underscore-min.js"></script>
+<script src="./simple_require.js"></script>
+<script src="./jan.js"></script>
+ 
+<script>
+function hoge(){
+  var jan = require( 'jan' );
+ 
+  ...janパッケージを利用したコード...
+  console.log( jan.PaiId.MAX ); // 例
+ 
+}
+</script> 
+ 
+*/
+window.underscore = _; // underscore.jsは特別に登録する
+function require(module){ return window[module.replace(/[.¥/]+/g,'')]; }
+
