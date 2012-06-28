0a657228c16ea71656a858381df2aeb25c2cdeb4
Harada Makoto <haramako@gmail.com>
2012-06-28 10:52:39 +0900

    no message

--- rparsec/test.rb
+++ rparsec/test.rb
@@ -3,32 +3,64 @@
 require 'rparsec'
 require 'pp'
 
-include RParsec
+class TestParser
+  include RParsec
+  include RParsec::Parsers
 
-NUM = Parsers.number
-WORD = Parsers.word.token(:word)
-OPEN_PAREN = Parsers.string('(')
-CLOSE_PAREN = Parsers.string(')')
+  attr_reader :lexer
 
-def op( token )
-  Parsers.string( token ) >> proc do |left,right|
-    [ token, left, right]
+  def initialize
+
+    # lexer
+    @keyword = Keywords.case_sensitive %w( var function ), word.token(:id)
+    @sym = Operators.new %w( ; ( ) [ ] { } )
+    @op = Operators.new %w( + - * / = )
+    @lexer = ( @keyword.lexer | number.token(:num) | @op.lexer | @sym.lexer ).lexeme
+
+    # program parser
+    op_parser = proc do |op|
+      @op[ op ] >> proc { |left,right| [ Token.new( op, op.to_s ), left, right ] }
+    end
+    op_table = OperatorTable.new do |t|
+      t.infixl op_parser.(:'='), 0
+      t.infixl op_parser.(:+), 10
+      t.infixl op_parser.(:-), 10
+      t.infixl op_parser.(:*), 20
+      t.infixl op_parser.(:/), 20
+    end
+
+    @term           = token(:num) | token(:id)
+    @exp            = Expressions.build( @term, op_table )
+    @statement      = @exp << token(:';')
+    @statement_list = @statement.many
+    @block          = token(:'{') >> @statement_list << token(:'}')
+    @function_decl  = sequence( @keyword[:function], token(:id), token(:'('), token(:')'), @block.optional ){ |_,id,_,_,body| [:function, id, body] }
+    @program = @function_decl
+
+    @parser = @lexer.nested( @program << eof )
   end
+
+  def parse( str )
+    @parser.parse str
+  end
+
+  private
+
+  alias s sequence
+
+  def watch_this
+    watch { |x| puts x }
+  end
+
 end
 
-op_table = OperatorTable.new do |t|
-  t.infixl op('+'), 10
-  t.infixl op('-'), 10
-  t.infixl op('*'), 20
-  t.infixl op('/'), 20
-end
 
+parser = TestParser.new
+# pp parser.lexer.parse ' a + 1 var '
 
-TERM = NUM | WORD | OPEN_PAREN >> Parsers.lazy{ EXP } << CLOSE_PAREN
-EXP = Expressions.build( TERM, op_table )
-
-EXP_LIST = EXP.many
-
-pp EXP_LIST.parse( 'hoge-(1+fuga)fuga' )
-
-pp EXP_LIST.parse( 'hoge' )
+pp parser.parse <<EOT
+function hoge(){
+  a = a + 1;
+  b = 2;
+}
+EOT
