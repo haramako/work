c029440411aa74f092ba10d9f2fa0fef996a6add
HARADA Makoto <haramako@gmail.com>
2012-06-30 16:11:08 +0900

    .

--- rparsec/test.rb
+++ rparsec/test.rb
@@ -3,32 +3,59 @@
 require 'rparsec'
 require 'pp'
 
-include RParsec
+class TestParser
+  include RParsec
+  extend RParsec::Parsers
 
-NUM = Parsers.number
-WORD = Parsers.word.token(:word)
-OPEN_PAREN = Parsers.string('(')
-CLOSE_PAREN = Parsers.string(')')
+  SEP           = string(/;/)
+  NUM           = number
+  WORD          = word.token(:word)
+  OPEN_PAREN    = string('(')
+  CLOSE_PAREN   = string(')')
+  OPEN_BRACKET  = string('{')
+  CLOSE_BRACKET = string('}')
+  OPERATORS     = Operators.new %w( + - = )
+  KEYWORDS      = Keywords.case_sensitive %w( function static )
 
-def op( token )
-  Parsers.string( token ) >> proc do |left,right|
-    [ token, left, right]
+  Lexer         = number.token(:number) | KEYWORDS.lexer | OPERATORS.lexer
+  def self.k
+    KEYWORDS
   end
+
+  def self.op( token )
+    string( token ) >> proc do |left,right|
+      [ token, left, right]
+    end
+  end
+
+  op_table = RParsec::OperatorTable.new do |t|
+    t.infixl op('+'), 1
+    t.infixl op('-'), 1
+    t.infixl op('*'), 2
+    t.infixl op('/'), 2
+    t.infixl op('='), 3
+  end
+
+  TERM = NUM | WORD | OPEN_PAREN >> lazy{ EXP } << CLOSE_PAREN
+  EXP = RParsec::Expressions.build( TERM, op_table )
+  STATEMENT = EXP << SEP
+  STATEMENT_LIST = STATEMENT.many
+  BLOCK = OPEN_BRACKET >> STATEMENT_LIST << CLOSE_BRACKET
+
+  FUNCTION_DECL = sequence( k[:function], WORD, OPEN_PAREN, CLOSE_PAREN, BLOCK ) { |f,id,_,_,body| [:function, id,body] }
+
+  def self.parse( str )
+    FUNCTION_DECL.parse( str )
+  end
+
 end
 
-op_table = OperatorTable.new do |t|
-  t.infixl op('+'), 10
-  t.infixl op('-'), 10
-  t.infixl op('*'), 20
-  t.infixl op('/'), 20
-end
 
+pp TestParser.k.lexer.lexeme.parse( 'static function hoge fuga' ) #.map(&:text)
 
-TERM = NUM | WORD | OPEN_PAREN >> Parsers.lazy{ EXP } << CLOSE_PAREN
-EXP = Expressions.build( TERM, op_table )
+#pp TestParser.parse <<EOT
+#functionhoge(){x=hoge-(1+fuga);y=hoge;}
+#EOT
 
-EXP_LIST = EXP.many
+# pp parser.parse( 'hoge;' )
 
-pp EXP_LIST.parse( 'hoge-(1+fuga)fuga' )
-
-pp EXP_LIST.parse( 'hoge' )
