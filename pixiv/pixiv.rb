5e5bf2c677fc49fc0313f5948f64011812291d2b
Makoto Harada <haramako@gmail.com>
2013-02-05 19:16:38 +0900

    .

--- pixiv/pixiv.rb
+++ pixiv/pixiv.rb
@@ -1,97 +1,67 @@
 # -*- coding: utf-8 -*-
-PIXIV_URL = URI('http://www.pixiv.net/')
+require_relative 'walker'
 
-def login_pixiv
-#  puts @@agent.methods
-  @@agent.cookie_jar.load 'cookie.yaml'
-  @@agent.cookie_jar.jar['pixiv.net']['/']['PHPSESSID']
-rescue
-  puts "login pixiv"
-  @@agent.post 'http://www.pixiv.net/login.php', { mode: 'login', pixiv_id: 'haramako', pass: 'mako0522', skip: 1 }
-  @@agent.cookie_jar.save_as 'cookie.yaml'
-end
+class Pixiv < Walker
 
+  PIXIV_URL = URI('http://www.pixiv.net/')
 
-# ユーザーページ
-walker %r(^http://www\.pixiv\.net/member.php\?id=(\d+)) do |url,match|
-  login_pixiv
-  page = get url
-
-  page.search('a').each do |e|
-    href = e.attr('href')
-    add_url PIXIV_URL+href[1..-1] if /\/member_illust\.php\?id=(\d)+/ === href
+  def login_pixiv
+    #  puts @@agent.methods
+    @agent.cookie_jar.load 'cookie.yaml'
+    @agent.cookie_jar.jar['pixiv.net']['/']['PHPSESSID']
+  rescue
+    puts "login pixiv"
+    @agent.post 'http://www.pixiv.net/login.php', { mode: 'login', pixiv_id: 'haramako', pass: 'mako0522', skip: 1 }
+    @agent.cookie_jar.save_as 'cookie.yaml'
   end
 
-  { expire: 60*60*24*365 }
-end
+  # ユーザーページ
+  walker %r(^http://www\.pixiv\.net/member.php\?id=(\d+)) do |url,match|
+    login_pixiv
+    page = get url
 
+    page.search('a').each do |e|
+      href = e.attr('href')
+      add_url PIXIV_URL+href[1..-1] if /\/member_illust\.php\?id=(\d)+/ === href
+    end
 
-# イラスト一覧ページ
-walker %r(http://www\.pixiv\.net/member_illust\.php\?id=(\d+)(&p=(\d+))?) do |url,match|
-  login_pixiv
-  page_no = match[3] || 1
-  page = get url
-  page.search('a').each do |e|
-    href = e.attr('href')
-    add_url PIXIV_URL+e.attr('href') if /member_illust\.php\?id=(\d+)&p=(\d+)/ === e.attr('href')
-    add_url PIXIV_URL+e.attr('href') if /member_illust\.php\?mode=medium&illust_id=(\d+)/ === e.attr('href')
-  end
-  { expire: 60*60*24*365 }
-end
-
-# イラストページ
-walker %r(^http://www\.pixiv\.net/member_illust.php\?mode=medium&illust_id=(\d+)) do |url,match|
-  login_pixiv
-  page = get url
-  id = match[1]
-  title = page.search('h1.title')[0].text
-  author = page.search('div.user-unit h1').text
-
-  if /mode=manga/ === page.search('div.works_display a').attr('href').to_s
-    # TODO: 漫画モードは未対応
-  else
-    # 通常のイラスト
-    image_url = page.search('div.works_display img')[0].attr('src').gsub( /_m\.jpg$/, '.jpg' )
-    image = get image_url
-
-    open( "#{IMG_DIR}#{id}.jpg", 'w' ){ |f| f.write image }
-    db.prepare('REPLACE images ( id, title, author ) VALUES ( ?, ?, ? )').execute( id, title, author )
+    { expire: 60*60*24*365 }
   end
 
-  { expire: 60*60*24*365 }
+  # イラスト一覧ページ
+  walker %r(http://www\.pixiv\.net/member_illust\.php\?id=(\d+)(&p=(\d+))?) do |url,match|
+    login_pixiv
+    page_no = match[3].to_i || 1
+    page = get url
+    page.search('a').each do |e|
+      href = e.attr('href')
+      add_url PIXIV_URL+e.attr('href') if /member_illust\.php\?id=(\d+)&p=(\d+)/ === e.attr('href')
+      add_url PIXIV_URL+e.attr('href') if /member_illust\.php\?mode=medium&illust_id=(\d+)/ === e.attr('href')
+    end
+    { expire: 60*60*24*365 }
+  end
+
+  # イラストページ
+  walker %r(^http://www\.pixiv\.net/member_illust.php\?mode=medium&illust_id=(\d+)) do |url,match|
+    login_pixiv
+    page = get url
+    id = match[1]
+    title = page.search('h1.title')[0].text
+    author = page.search('div.user-unit h1').text
+
+    if /mode=manga/ === page.search('div.works_display a').attr('href').to_s
+      # TODO: 漫画モードは未対応
+    else
+      # 通常のイラスト
+      image_url = page.search('div.works_display img')[0].attr('src').gsub( /_m\.(jpg|png|gif)$/){|x| x[2..-1] }
+      image = get image_url
+
+      ext = File.extname(image_url).gsub(/\?.+$/,'')
+      open( project_path + (id.to_s + ext), 'w' ){ |f| f.write image }
+      # db.prepare('REPLACE images ( id, title, author ) VALUES ( ?, ?, ? )').execute( id, title, author )
+    end
+
+    { expire: 60*60*24*365 }
+  end
+
 end
-
-
-#add_url 'http://www.pixiv.net/member.php?id=10264'
-
-add_url 'http://www.pixiv.net/member.php?id=10264'
-walk 'http://www.pixiv.net/member_illust.php?id=10264'
-
-walk 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=7153283'
-
-#walk 'http://www.pixiv.net/member_illust.php?id=396179'
-#walk 'http://www.pixiv.net/member_illust.php?id=396179&p=2'
-# walk 'http://i2.pixiv.net/img20/img/maim2/29198523.jpg'
-#walk 'http://i2.pixiv.net/img20/img/maim2/26939305.jpg'
-#walk 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=26939305'
-#walk 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=1002898'
-
-#walk 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=6517473'
-
-add_url 'http://www.pixiv.net/member.php?id=84409'
-walk_around
-
-# walk 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=29215927'
-
-=begin
-add_url 'http://www.pixiv.net/member.php?id=133790'
-add_url 'http://www.pixiv.net/member.php?id=5238'
-add_url 'http://www.pixiv.net/member.php?id=169083'
-add_url 'http://www.pixiv.net/member.php?id=43614'
-add_url 'http://www.pixiv.net/member.php?id=14063'
-add_url 'http://www.pixiv.net/member.php?id=9731'
-add_url 'http://www.pixiv.net/member.php?id=417796'
-add_url 'http://www.pixiv.net/member.php?id=2200242'
-add_url 'http://www.pixiv.net/member.php?id=257090'
-add_url 'http://www.pixiv.net/member.php?id=101608'
-=end
--- pixiv/walker.rb
+++ pixiv/walker.rb
@@ -6,77 +6,77 @@ require 'digest/sha1'
 require 'fileutils'
 require 'mysql'
 require 'uri'
+require 'pathname'
 
-CACHE_DIR = '/tmp/walker/cache/'
-IMG_DIR = '/tmp/walker/img/'
+CACHE_DIR = Pathname('/tmp/walker/cache/')
+IMG_DIR = Pathname('/var/walker/img/')
+FileUtils.mkdir_p IMG_DIR
 
 $cache_enable = true
 
-module Walker
-
+module Handler
   module_function
 
-  @@agent = Mechanize.new
-  @@agent.request_headers['Accept-Language'] = 'ja,en-US'
-  @@walkers = []
+  def db
+    return @@db if defined? @@db
+    @@db = Mysql.new( 'localhost', 'root', nil, 'walker' )
+  end
 
-  def add_url( url )
+  def add_url( project, url )
     puts "add_url: #{url}"
     url = url.to_s
     db.query 'BEGIN'
+
+    # プロジェクトを取得する
+    res = db.prepare('SELECT id FROM projects WHERE url = ?').execute( project.url )
+    if res.num_rows > 0
+      row = res.fetch
+      project.id = row[0]
+    else
+      db.prepare( 'INSERT INTO projects ( url, name, created_at ) VALUES ( ?, ?, NULL )' ).execute( url, project.name )
+      project.id = db.insert_id
+    end
+
     if db.prepare('SELECT COUNT(*) FROM urls WHERE url = ?').execute( url ).fetch[0].to_i == 0
-      db.prepare( 'INSERT INTO urls ( url, expire_at ) VALUES ( ?, NOW() )' ).execute( url )
+      db.prepare( 'INSERT INTO urls ( url, project_id, expire_at, created_at ) VALUES ( ?, ?, NOW(), NULL )' ).execute( url, project.id )
     end
     db.query 'COMMIT'
   end
 
-  def walker( regexp, &block )
-    @@walkers << { regexp: regexp, block: block }
+  @@walkers = []
+
+  def self.walker( regexp, _class, &block )
+    @@walkers << { _class: _class, regexp: regexp, block: block }
   end
 
-  def get( url )
-    FileUtils.mkdir_p CACHE_DIR
-    filename = Digest::SHA1.hexdigest(url)
-    path = CACHE_DIR+filename
-    if $cache_enable and File.exists? path
-      html = File.open( path, 'r:utf-8' ){|f| f.read }
-      Nokogiri::HTML( html )
-    else
-      puts "downloading #{url}"
-      page = @@agent.get url, [], 'http://www.pixiv.net/'
-      open( path, 'w' ){|f| f.write page.body }
-      case page
-      when Mechanize::Image
-        page.body
-      else
-        page.root
-      end
-    end
-  end
-
-  def walk( url )
+  def self.walk( project, url )
     puts "walk: #{url}"
     @@walkers.each do |w|
       if match = w[:regexp].match( url )
-        w[:block].call url, match
-        break
+        obj = w[:_class].new( project, url )
+        obj.instance_exec( url, match,  &w[:block] )
+        return
       end
     end
+    puts "no url match for #{url}"
   rescue
     raise $!
     # STDERR.puts $!, $!.backtrace
   end
 
-  def walk_around
+  def self.walk_around
     while true
       begin
         db.query 'BEGIN'
         row =  db.query("SELECT * FROM urls WHERE status = '' ORDER BY expire_at LIMIT 1 FOR UPDATE" ).fetch_hash
         break unless row
         db.prepare("UPDATE urls SET status = 'P' WHERE url = ? ").execute( row['url'] )
-        walk row['url']
+        project_row =  db.prepare("SELECT id, name, url FROM projects WHERE id = ?").execute(row['project_id']).fetch
+        project = Project.new( project_row[1], project_row[2] )
+        project.id = project_row[0]
+        walk project, row['url']
         db.commit
-      rescue Mechanize::ResponseCodeError
+      rescue Mechanize::ResponseCodeError, Errno::ETIMEDOUT
         db.prepare("UPDATE urls SET status = 'F' WHERE url = ? ").execute( row['url'] )
         db.commit
         pp $!
@@ -87,18 +87,90 @@ module Walker
     end
   end
 
-  private 
-  
-  def db
-    return @@db if defined? @@db
-    @@db = Mysql.new( 'localhost', 'root', nil, 'walker' )
+end
+
+class Project
+  attr_accessor :id
+  attr_reader :url, :name
+
+  def initialize( name, url )
+    @id = nil
+    @name = name
+    @url = url
+  end
+
+  def self.default
+    project = Project.new( '', 'unknown' )
+    project.id = 1
+    project
   end
 
 end
 
-include Walker
+class Walker
 
+  attr_reader :project, :url
+
+  def initialize( project, url )
+    @project = project
+    @cur_url = url
+    @agent = Mechanize.new
+    @agent.request_headers['Accept-Language'] = 'ja,en-US'
+  end
+
+  def get( url )
+    if @cur_url.to_s.match %r(^http://g\.e-hentai\.org/)
+      sleep 1
+    end
+
+    FileUtils.mkdir_p CACHE_DIR
+    filename = Digest::SHA1.hexdigest(url)
+    path = CACHE_DIR+filename
+    if $cache_enable and File.exists? path
+      html = File.open( path, 'r:utf-8' ){|f| f.read }
+      if html[0] == '<'
+        Nokogiri::HTML( html )
+      else
+        html
+      end
+    else
+      puts "downloading #{url}"
+      page = @agent.get url, [], 'http://www.pixiv.net/'
+
+      open( path, 'w' ){|f| f.write page.body }
+      case page
+      when Mechanize::Image
+        page.body
+      else
+        html = page.root.to_s
+        if html.match(/The ban expires/) # Banが終わるまで待つ
+          puts 'wait!'
+          sleep 60*60
+          get( url )
+        end
+
+        page.root
+      end
+    end
+  end
+
+  def project_path
+    path = IMG_DIR + "%06d"%[project.id]
+    FileUtils.mkdir_p path
+    path
+  end
+
+  def add_url( url )
+    Handler.add_url @project, url
+  end
+
+  def self.walker( regexp, &block )
+    Handler.walker regexp, self, &block
+  end
+
+end
 
 # require_relative 'taikai'
 require_relative 'pixiv'
+# require_relative 'e-hentai'
 
